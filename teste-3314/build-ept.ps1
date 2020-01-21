80bc8e6d8c5e1355d92aebe45aaf8cc6af00bc90
Fernando Gomes <feromes@gmail.com>
2020-01-21 17:57:06 -0300

    Criando a build de Teste do Entwine

--- teste-3314/build-ept.ps1
+++ teste-3314/build-ept.ps1
@@ -1,6 +1,6 @@
 . ..\env.ps1
-$env:GDAL_DATA = $($env:HOMEPATH + "AppData\Local\Continuum\miniconda3\Library\share\gdal\")
-$env:PROJ_LIB = $($env:HOMEPATH + "AppData\Local\Continuum\miniconda3\Library\share\proj\")
+$env:GDAL_DATA = $($env:HOMEPATH + "\AppData\Local\Continuum\miniconda3\Library\share\gdal\")
+$env:PROJ_LIB = $($env:HOMEPATH + "\AppData\Local\Continuum\miniconda3\Library\share\proj\")
 $inicio = Get-Date
 entwine build -i $target -o $($source + '\entwine-3314') -t 3 -v --tmp $env:TMP
 $termino = Get-Date
