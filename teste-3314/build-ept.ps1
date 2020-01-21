cd7577bae1e24f4d6069419223023e3f9495d6a1
Fernando Gomes <feromes@gmail.com>
2020-01-21 17:16:49 -0300

    Criando a build de Teste do Entwine

---
+++ teste-3314/build-ept.ps1
@@ -0,0 +1,7 @@
+. ..\env.ps1
+$inicio = Get-Date
+#entwine build -i $target -o $($source + '\entwine-3314') -t 3 -v --tmp $env:TMP
+$termino = Get-Date
+echo $termino
+$duracao = $termino - $inicio
+echo $('Duracao ' + $duracao.Hours + ' horas e ' + $duracao.Minutes + 'minutos!')
